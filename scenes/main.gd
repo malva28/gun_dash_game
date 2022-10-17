3ab1f29537be16ee350fb5e584f47493406eb58e
malva28 <valentina.garrido@ug.uchile.cl>
2022-10-16 23:59:56 -0300

    test movimiento torre del fondo

---
+++ scenes/main.gd
@@ -0,0 +1,18 @@
+extends Spatial
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+onready var dash_chara = $MainChara
+onready var tower = $Tower
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	tower.bind_player(dash_chara)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
