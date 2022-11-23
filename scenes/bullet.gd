214d341ead67c758509abb4e89ca9656d60b29d9
malva28 <valentina.garrido@ug.uchile.cl>
2022-11-23 01:59:40 -0300

    Bullet reload when defeating enemies

--- scenes/bullet.gd
+++ scenes/bullet.gd
@@ -8,11 +8,14 @@ var collision_info = null
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-
+var player_instance = null
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
+	
+func bind_player(player_inst):
+	player_instance = player_inst
 	
 func _physics_process(delta):
 	collision_info = move_and_collide(Velocity.normalized()*SPEED)
@@ -22,7 +25,8 @@ func _physics_process(delta):
 		if collider.has_method("get_collision_layer"):
 			layer = collider.get_collision_layer()
 			if layer == 1:
-					queue_free()
+				print(collider)
+				queue_free()
 			elif layer == 2:
 					if collider.has_method("_resolve_body_enter"):
 						collider._resolve_body_enter(self)
@@ -36,6 +40,13 @@ func _physics_process(delta):
 
 
 	translation += SPEED * transform.basis.x * delta
+
+func _resolve_area_enter(area: Area):
+	if area.has_method("_bullet_enter"):
+		area._bullet_enter(self)
+
+func _enemy1_enter(enemy1):
+	queue_free()
 
 func _main_chara_enter(main_chara):
 	main_chara.hud.half_heart_damage()
--- scenes/enemy1.gd
+++ scenes/enemy1.gd
@@ -5,6 +5,8 @@ extends Area
 # var a = 2
 # var b = "text"
 
+onready var extra_bullet = $ExtraBullet
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -13,6 +15,21 @@ func _ready():
 func _on_body_entered(body: Node):
 	if body.has_method("_resolve_area_enter"):
 		body._resolve_area_enter(self)
+		
+func _resolve_body_enter(body: Node):
+	if body.has_method("_enemy1_enter"):
+		body._enemy1_enter(self)
+		
+func _bullet_enter(bullet: KinematicBody):
+	bullet.player_instance.hud.reload_one()
+	extra_bullet.play_extra_ammo_anim_and_despawn()
+	
+	# TODO: agregar animacion de morir del enemigo despues
+	yield(extra_bullet.anim_player, "animation_finished")
+	
+	if bullet.has_method("_enemy1_enter"):
+		bullet._enemy1_enter(self)
+	queue_free()
 		
 func _main_chara_enter(main_chara: KinematicBody):
 	# Health loss
