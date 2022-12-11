6a06c95e4c579d9a373e5da7ec9bffbeb1dfa8af
AntonioTorga <82778424+AntonioTorga@users.noreply.github.com>
2022-12-11 16:40:29 -0300

    DOPE! arreglado el transform de lvl_af y spike

--- scenes/main_chara.gd
+++ scenes/main_chara.gd
@@ -84,14 +84,14 @@ func _physics_process(delta):
 
 	
 	### SI CAE EN UN SPIKE POR ENCIMA RECIBE DAÑO
-#	for i in get_slide_count():
-#		var collision_info = get_slide_collision(i)
-#		if collision_info.collider.has_method("get_collision_layer"):
-#			var layer = collision_info.collider.get_collision_layer()
-#			if layer == 33 and collision_info.normal.y > 0:
-#				print(layer)
-#				print(collision_info.normal.y)
-#				hud.hp -= 1
+	for i in get_slide_count():
+		var collision_info = get_slide_collision(i)
+		if collision_info.collider.has_method("get_collision_layer"):
+			var layer = collision_info.collider.get_collision_layer()
+			if (layer>>5)==1 and collision_info.normal.y > 0.9:
+				velocity.y = 3
+				hud.whole_heart_damage()
+				main_chara_death()
 
 		
 	### SI ESTA EN EL SUELO, RECARGA
