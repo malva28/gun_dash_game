2ceb8f6581a6326db0818b1949e153f4ffc26a0f
crimson98 <felipe.borjas@ug.uchile.cl>
2022-12-12 17:55:04 -0400

    menu pausa, creditos

---
+++ Pause_Menu.gd
@@ -0,0 +1,41 @@
+extends MarginContainer
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+onready var resume = $"%Resume"
+onready var mainmenu = $"%MainMenu"
+onready var quit = $"%Quit"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	resume.connect("pressed", self, "_on:resume")
+	mainmenu.connect("pressed", self, "_on_mainmenu")
+	quit.connect("pressed", self, "_on_quit")
+	
+	hide()
+	
+	
+	
+	
+func _input(event):
+	if event.is_action_pressed("pause"):
+		visible = !visible
+		get_tree().paused = visible
+	
+	
+func _on_resume():
+	hide()
+	get_tree().paused = false
+	
+func _on_mainmenu():
+	get_tree().paused = false
+	get_tree().change_scene("res://scenes/ui/main_menu.tscn")
+	
+func _on_quit():
+	get_tree().quit()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
--- scenes/ui/main_menu.gd
+++ scenes/ui/main_menu.gd
@@ -20,5 +20,5 @@ func _on_exit_pressed():
 	get_tree().quit()
 	
 func _on_credits_pressed():
-	print("aaaaa")
+	get_tree().change_scene("res://scenes/ui/Credits.tscn")
 	
