5bd61b8a752b17ef1f072e0d4812d2bab0c4ee98
malva28 <valentina.garrido@ug.uchile.cl>
2022-12-12 23:16:21 -0300

    added deathbox to level_af

--- scenes/main.gd
+++ scenes/main.gd
@@ -1,17 +1,9 @@
 extends Spatial
-onready var character = $MainChara
 
 onready var main_chara = $MainChara
 onready var deathbox = $Deathbox
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
 
 func _ready():
-	print("im ready")
-	print(main_chara)
-	print(deathbox)
-	#deathbox.connect("body_entered", main_chara, "_on_Deathbox_body_entered", [deathbox])
 	deathbox.connect("body_entered", main_chara, "_on_Deathbox_body_entered", [deathbox])
 
